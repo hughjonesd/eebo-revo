d37cf02eca5e130f2b051ed3b2f6e87f3bbab052
David Hugh-Jones <davidhughjones@gmail.com>
2022-01-17 12:14:25 +0000

    tweak download sh

--- download-eebo-zips.sh
+++ download-eebo-zips.sh
@@ -5,9 +5,13 @@
 
 eebo_zips_url="https://ucd02cc615acf8e7f1719d158a3e.dl.dropboxusercontent.com/zip_download_get/BBU2AXBLG_dQjthH_rC07-XRE_I1PXOCjURD28nbkuwjprvfE3v_pqFOUex9SUAhhrm-cmj0YBq-cJYlAm2UjIjtuTEPxlwWYJHo9NQmujvkqQ?_download_id=1461240365575096507288853614259105910800945992200607443741746527044&_notify_domain=www.dropbox.com&dl=1"
 
-wget -O "eebo-zips-from-dropbox.zip" $eebo_zips_url
+# wget -O "data-raw/eebo-zips-from-dropbox.zip" $eebo_zips_url
+# or
+curl -o "data-raw/eebo-zips-from-dropbox.zip" $eebo_zips_url
 
 # check file integrity
-unzip -t "eebo-zips-from-dropbox.zip"
+unzip -t "data-raw/eebo-zips-from-dropbox.zip"
 
-unzip "eebo-zips-from-dropbox.zip"
+mkdir -p "data-raw/eebo-zips"
+# you can use the -u option to only update files if you had a partial download
+unzip -d "data-raw/eebo-zips" "data-raw/eebo-zips-from-dropbox.zip" "*P5_XML_OXF*.zip"
