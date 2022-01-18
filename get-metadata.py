4c6c0059f9378bf1b0d103ae3ab7b338426e04d5
David Hugh-Jones <davidhughjones@gmail.com>
2022-01-18 13:19:36 +0000

    Parallelize get-metadata.py

--- get-metadata.py
+++ get-metadata.py
@@ -3,26 +3,34 @@
 import sys
 import os
 import pandas as pd
+import multiprocessing as mp
 from process_tei import get_xml_from_line, get_metadata
 
-metadata_path = "data/metadata.csv"
-
-metadata_df = pd.DataFrame(columns = ["id", "date", "author", 
-                    "title", "pub_place", "lang"])
-md_list = []
-
-for line in sys.stdin.readlines():
+def process_line(line):
     line = line.rstrip()
     print(line)
     xml = get_xml_from_line(line)
     md = get_metadata(xml)
-    md_list.append(md)
+    return md
 
-metadata_df = metadata_df.append(md_list)
+if __name__ == "__main__":
+    metadata_path = "data/metadata.csv"
 
-write_header = not os.path.exists(metadata_path)
-with open(metadata_path, 'a', newline = '') as mf:
-    metadata_df.to_csv(mf, index = False, header = write_header)
+    metadata_df = pd.DataFrame(columns = ["id", "date", "author", 
+                        "title", "pub_place", "lang"])
+    md_list = []
+
+    pool = mp.Pool(mp.cpu_count())
+
+    md_list = pool.map(process_line, sys.stdin.readlines())
+
+    pool.close()
+
+    metadata_df = metadata_df.append(md_list)
+
+    write_header = not os.path.exists(metadata_path)
+    with open(metadata_path, 'a', newline = '') as mf:
+        metadata_df.to_csv(mf, index = False, header = write_header)
 
 
 
