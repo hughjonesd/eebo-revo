d9820b59d9b2760c191c5f88c08b23e12fc51606
David Hugh-Jones <davidhughjones@gmail.com>
2022-01-18 12:35:44 +0000

    don't print non-english texts in get_text()

--- process_tei.py
+++ process_tei.py
@@ -61,6 +61,10 @@ def get_text(xml):
     """Takes a TCP XML document and returns just the original text"""
     doc = etree.fromstring(xml)
 
+    lang = doc.teiHeader.profileDesc.langUsage.language.get("ident")
+    if lang != "eng":
+        raise StopIteration
+
     para_tags = ["item", "p", "label", "postscript", "q", "salute",
                  "signed", "sp", "argument", "opener", "closer", "head",
                  "note", "div1", "div2", "div3", "div4", "div5", 
@@ -76,7 +80,7 @@ def get_text(xml):
     para_tags = [tei_ns + t for t in para_tags]
     ignore_tags = [tei_ns + t for t in ignore_tags]
     delete_tags = [tei_ns + t for t in delete_tags]
-    for text in doc.findall(".//{*}text"):
+    for text in doc.findall(".//{*}text[@lang='eng' or @lang='unk']"):
         body = text.find(".//{*}body") # only one, we hope
         for eol_hyphen in body.findall(".//{*}g[@ref='char:EOLhyphen']"):
             eol_hyphen.clear(keep_tail = True)
