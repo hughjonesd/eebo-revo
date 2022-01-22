6f5be9b8cd21a1284b275a4f7a3b312b928e1b05
David Hugh-Jones <davidhughjones@gmail.com>
2022-01-22 15:58:07 +0000

    add sbatch files to run-fasttext; make it work on ada

--- run-fasttext.sh
+++ run-fasttext.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=davidhughjones@gmail.com
+#SBATCH --partition=compute-24-128
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=120G
+#SBATCH -o run-fasttext.%j.out
+#SBATCH -e run-fasttext.%j.err
+
+# create fastText embeddings using all the training files
+
 # about how many files to randomly select for input
 n_files=5000
+
+# number of threads to use in fasttext
+n_threads=1
 
 # seed RANDOM number generator
 RANDOM=1027
@@ -16,4 +30,4 @@ for f in data/texts/*; do
     cat $f >> data/fasttext-input
   fi
 done
-./fastText-0.9.2/fasttext skipgram -input data/fasttext-input -output data/fasttext-vectors -thread 4 -epoch 1 -minCount 20 -dim 500 
+./fastText-0.9.2/fasttext skipgram -input data/fasttext-input -output data/fasttext-vectors -thread $n_threads -epoch 1 -minCount 20 -dim 300 
